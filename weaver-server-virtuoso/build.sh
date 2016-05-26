798194d8bc159c5a4db962a10745c32cd83fde57
Gijs van der Ent <gijs@sysunite.com>
2016-05-26 11:18:22 +0200

    Adds the ability to build an image from master, instead of requiring a release

--- weaver-server-virtuoso/build.sh
+++ weaver-server-virtuoso/build.sh
@@ -2,17 +2,25 @@
 
 set -e
 
-if [[ "$#" -lt 1 ]]; 
+if [[ "$1" -eq "--help" ]]; 
 then
-  echo "Usage: $0 <release tag> [tag]"
+  echo "Usage: $0 [release tag] [tag]"
   echo "The release needs to be properly tagged on github"
   exit 1
 fi;
 
-VERSION="$1"
+if [[ -z "${1}" ]]; 
+then
+  VERSION="master"
+  DLVERSION="${VERSION}"
+else
+  VERSION="${1}"
+  DLVERSION="v${VERSION}"
+fi;
+
 TAG="$2"
 PROJECT="weaver-server-virtuoso"
-SRC_LOC="https://github.com/weaverplatform/${PROJECT}/archive/v${VERSION}.tar.gz"
+SRC_LOC="https://github.com/weaverplatform/${PROJECT}/archive/${DLVERSION}.tar.gz"
 
 curl --location --output code.tar.gz "${SRC_LOC}"
 
@@ -35,6 +43,6 @@ docker build -t "${DOCKER_PACKAGE}" .
 rm -rf code
 
 
-echo "${DOCKER_PACKAGW} done building and ready to be distributed. "
+echo "${DOCKER_PACKAGE} done building and ready to be distributed. "
 echo "Test it, and after that run the command: docker push ${DOCKER_PACKAGE}"
 echo "to publish the image."
